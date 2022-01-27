b4a595817619992972479d7b284131afd1e31e90
SpencerKing7 <spencerking.c@gmail.com>
2022-01-26 22:09:51 -0700

    Add project files.

---
+++ MovieCollectionApp/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MovieCollectionApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieCollectionApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private MovieInputContext movieContext { get; set; } 
+
+        // Constructor
+        public HomeController(ILogger<HomeController> logger, MovieInputContext name)
+        {
+            _logger = logger;
+            movieContext = name;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Podcast()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult EnterMovie ()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult EnterMovie (MovieInputModel ar)
+        {
+            movieContext.Add(ar);
+            movieContext.SaveChanges();
+
+            return View("Confirmation", ar);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MovieCollectionApp/Models/MovieInputContext.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieCollectionApp.Models
+{
+    public class MovieInputContext : DbContext
+    {
+        public MovieInputContext(DbContextOptions<MovieInputContext> options) : base(options)
+        {
+            //blank 
+        }
+
+        public DbSet<MovieInputModel> responses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<MovieInputModel>().HasData(
+
+                new MovieInputModel
+                {
+                    MovieId = 1,
+                    Category = "Action/Adventure",
+                    Title = "Lord of the Rings - Return of the King",
+                    Year = 2003,
+                    Director = "Peter Jackson",
+                    Rating = "PG-13",
+                    Edited = true,
+                    LentTo = null,
+                    Notes = "Best movie ever."
+                },
+                new MovieInputModel
+                {
+                    MovieId = 2,
+                    Category = "Comedy",
+                    Title = "Get Smart",
+                    Year = 2008,
+                    Director = "Peter Segal",
+                    Rating = "PG-13",
+                    Edited = true,
+                    LentTo = "John Doe",
+                    Notes = "Funniest movie ever."
+                },
+                new MovieInputModel
+                {
+                    MovieId = 3,
+                    Category = "Action/Adventure",
+                    Title = "Interstellar",
+                    Year = 2014,
+                    Director = "Christopher Nolan",
+                    Rating = "PG-13",
+                    Edited = true,
+                    LentTo = "Sally",
+                    Notes = "Craziest movie ever."
+                }
+            );
+        }
+    }
+}
